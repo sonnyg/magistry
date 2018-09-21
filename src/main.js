449c006ebe6944ee40a4b2f8db10b7458c6ee889
sonnyg <sonny@evagabond.com>
2018-09-21 16:59:33 -0700

    refactor: move main.js
    moving main.js into src folder

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'src/index.html'),
+    pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
